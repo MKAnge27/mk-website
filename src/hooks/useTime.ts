438ffa62f4f6af1f837781ab83f148155461786b
MKcode27 <127638632+MKcode27@users.noreply.github.com>
2025-07-04 16:40:40 -0400

    fix: import path and use time

---
+++ src/hooks/useTime.ts
@@ -0,0 +1,34 @@
+
+import { useEffect, useMemo, useState } from "react";
+
+function useTime(refreshRate: number) {
+    // Set the default value as `Date.now()`
+    // causes hydration issues of mismatch text.
+    const [value, setValue] = useState<number>(0);
+
+    useEffect(() => {
+        if (typeof window == undefined) {
+            return;
+        }
+
+        const interval = setInterval(() => {
+            setValue(Date.now());
+        }, refreshRate);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [refreshRate]);
+
+    const currentTime = useMemo(() => {
+        return new Intl.DateTimeFormat("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+        }).format(value);
+    }, [value]);
+
+    return value ? currentTime : "00:00";
+}
+
+export default useTime;
